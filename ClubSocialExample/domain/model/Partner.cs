e5230c0dcb7e4c2c87cfcc8258a1ff0e9f141c26
MarlonGr15 <marlon.giral1511@gmail.com>
2025-08-20 19:13:05 -0500

    add project

---
+++ ClubSocialExample/domain/model/Partner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubSocialExample.domain.model
+{
+    internal class Partner:User
+    {
+        private long IdPartner;
+        private double amount;
+        private string type;
+        private DateTime dateCreated;
+
+        public long IdPartner1 { get => IdPartner; set => IdPartner = value; }
+        public double Amount { get => amount; set => amount = value; }
+        public string Type { get => type; set => type = value; }
+        public DateTime DateCreated { get => dateCreated; set => dateCreated = value; }
+    }
+}
